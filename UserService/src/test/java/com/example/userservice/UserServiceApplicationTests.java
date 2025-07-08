d4203bf69f24e5d888d39d644116ccddbb629ea1
V4d1mC0d3r <vadimgordikov78@gmail.com>
2025-07-08 16:28:23 +0500

    change of environment

--- UserService/src/test/java/com/example/userservice/UserServiceApplicationTests.java
+++ UserService/src/test/java/com/example/userservice/UserServiceApplicationTests.java
@@ -1,10 +1,10 @@
-package com.example.banking;
+package com.example.userservice;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class BankingApplicationTests {
+class UserServiceApplicationTests {
 
     @Test
     void contextLoads() {
